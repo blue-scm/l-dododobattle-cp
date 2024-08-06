b616364991910321c03565ee4574e52e4b10e03f
坪田沙采 <blue1993@tsubotasaaya.local>
2024-08-07 01:08:25 +0900

    FV調整、画像差し替え

--- build/config.js
+++ build/config.js
@@ -1,24 +1,24 @@
 // config
-const PROJECT_ROOT = "../html";
+const PROJECT_ROOT = '../html';
 
 // dockerでの立ち上げかどうかこのENVで判断
-let proxy = "localhost:8186"
+let proxy = 'localhost:8138';
 if (process.env.WEB_SERVER_PROXY) {
     // {compose.yamlのservicesで定義したapacheの名前}:{docker上で開かれているport}
-    proxy = process.env.WEB_SERVER_PROXY
+    proxy = process.env.WEB_SERVER_PROXY;
 }
 
 const CONFIG = {
     PATH: {
         root: PROJECT_ROOT,
-        ejs: PROJECT_ROOT + "/assets/ejs/",
-        ejs_json: PROJECT_ROOT + "/assets/ejs/_json/",
-        scss: PROJECT_ROOT + "/assets/scss/",
-        css: PROJECT_ROOT + "/assets/css/",
-        js: PROJECT_ROOT + "/assets/js/",
-        es6: PROJECT_ROOT + "/assets/es6/",
-        ts: PROJECT_ROOT + "/assets/ts/",
-        img: PROJECT_ROOT + "/assets/img/",
+        ejs: PROJECT_ROOT + '/assets/ejs/',
+        ejs_json: PROJECT_ROOT + '/assets/ejs/_json/',
+        scss: PROJECT_ROOT + '/assets/scss/',
+        css: PROJECT_ROOT + '/assets/css/',
+        js: PROJECT_ROOT + '/assets/js/',
+        es6: PROJECT_ROOT + '/assets/es6/',
+        ts: PROJECT_ROOT + '/assets/ts/',
+        img: PROJECT_ROOT + '/assets/img/',
     },
     OPTION: {
         proxy: proxy,
